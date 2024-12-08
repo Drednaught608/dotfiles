b69112b351886ddde1035d7d9abb85b02de73d42
Michael Hagan <62200341+Drednaught608@users.noreply.github.com>
2024-12-07 22:44:14 -0600

    Added original habamax theme in vim sync script

--- .scripts/sync_vim_plugins.sh
+++ .scripts/sync_vim_plugins.sh
@@ -7,8 +7,15 @@ NC='\033[0m' # No Color
 
 PLUGNUM="$(wc --lines ~/.vimplugins | grep -o '[0-9]*')"
 
+VIM1="$HOME/.vim"
+VIM2="$HOME/vimfiles"
+
 echo -e "\n${YELLOW}Starting sync of $PLUGNUM Vim plugins${NC}\n"
 mkdir -p ~/.vim/pack/plugins/start/ ~/vimfiles/pack/plugins/start/
+mkdir -p ~/.vim/colors/ ~/vimfiles/colors/
+
+curl -fso temp_file1 https://raw.githubusercontent.com/Drednaught608/vim-habamax/refs/heads/master/colors/habamax.vim && mv temp_file1 "$VIM1/colors/habamax.vim"
+curl -fso temp_file2 https://raw.githubusercontent.com/Drednaught608/vim-habamax/refs/heads/master/colors/habamax.vim && mv temp_file2 "$VIM2/colors/habamax.vim"
 
 counter=1
 p="$HOME/.vimplugins"
@@ -19,8 +26,8 @@ while read p; do
     echo -e "\n[${GREEN}PLUGIN $counter/$PLUGNUM${NC}: ${CYAN}$p${NC}]\n"
 
     DIR="${p##*/}"
-    PATH1="$HOME/.vim/pack/plugins/start"
-    PATH2="$HOME/vimfiles/pack/plugins/start"
+    PATH1="$VIM1/pack/plugins/start"
+    PATH2="$VIM2/pack/plugins/start"
 
     if [ -d "$PATH1/$DIR/.git" ]; then
         echo -ne "${CYAN}~/.vim:     ${NC}"
